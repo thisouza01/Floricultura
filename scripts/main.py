3be2f195a533920fd0c682e845dd1135d9f68ea5
unknown <tihsouza01@gmail.com>
2025-01-11 14:40:02 -0300

    Deixando mais legível o código

--- scripts/main.py
+++ scripts/main.py
@@ -19,49 +19,44 @@ try:
             
         funcao.mostra_linha() 
 
-
-        if escolha == 1: # CADASTRA AS PLANTAS
+##########################################################################
+#
+#                          CADASTRA AS PLANTAS
+#
+##########################################################################
+        if escolha == 1: 
             from banco_dados.banco import cadastrar_planta_db
 
             # adiciona planta ao banco de dados
             cadastrar_planta_db()
 
-        elif escolha == 2: # MOSTRA AS PLANTAS
-            import pandas as pd
+##########################################################################
+#
+#                          LISTA AS PLANTAS
+#
+##########################################################################
+        elif escolha == 2: 
             from banco_dados.banco import mostrar_plantas_db
 
+            funcao.mostra_cabecalho('SELECIONANDO...')
 
-            # escolha qual versão ver
-            escolha_lista = str(input('1 - Arquivo\n2 - Tabela do banco de dados \n--> '))
+            mostrar_plantas_db()
 
-            funcao.mostra_cabecalho('SELECIONANDO')
-
-            # verifica escolha
-            if escolha_lista.isnumeric() and (escolha_lista == '1' or escolha_lista == '2'):
-                escolha_lista = int(escolha_lista)
-            else:
-                print('Escolha uma opção válida!!')    
-
-            if escolha_lista == 1:
-                # carregar dados do excel
-                tabela = pd.read_excel(r"C:\Users\WIN 11\OneDrive\Desktop\OneDrivethiago\OneDrive\Excel-Python\Floricultura - Copiar.xlsx")
-
-                # ordenar por nome
-                tabela = tabela.sort_values(by="Nome")
-
-                # salvar alteração
-                tabela.to_excel(r"C:\Users\WIN 11\OneDrive\Desktop\OneDrivethiago\OneDrive\Excel-Python\Floricultura - Copiar.xlsx", index=False)
-                
-                # exibe os dados
-                print(tabela)  
-            else:
-                mostrar_plantas_db()
-                
-        elif escolha == 3: # CONVERTE O ARQUIVO
+##########################################################################
+#
+#                         CONVERTE PARA ARQUIVO
+#
+##########################################################################                
+        elif escolha == 3: 
             import conversao.converter_arquivo as convert_arq
 
             convert_arq.escreve_arquivo()  
             
+##########################################################################
+#
+#                              SAIDA
+#
+##########################################################################             
         elif escolha == 4:
             break
         else:
