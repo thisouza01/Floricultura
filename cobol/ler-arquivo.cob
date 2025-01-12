a25554067c78c92229b1b0e59f0901249bbcffa0
Thiago Souza <124224124+thisouza01@users.noreply.github.com>
2025-01-12 07:59:57 -0300

    Update ler-arquivo.cob
    Lendo corretamente, preciso pular os cabeçalhos agora

--- cobol/ler-arquivo.cob
+++ cobol/ler-arquivo.cob
@@ -7,7 +7,11 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. FLORICULTURA_ARQUIVO.
       * 
-       ENVIRONMENT DIVISION.
+       ENVIRONMENT DIVISION.   
+       CONFIGURATION SECTION.    
+           SPECIAL-NAMES.
+               DECIMAL-POINT IS COMMA.
+      * 
        INPUT-OUTPUT SECTION.
           FILE-CONTROL.
               SELECT ARQUIVO ASSIGN TO CAMINHO-ARQUIVO
@@ -21,13 +25,20 @@
       * 
        WORKING-STORAGE SECTION.
        01  CAMINHO-ARQUIVO PIC X(100) VALUE
-           'C:\Users\WIN 11\Floricultura\Floricultura-1' &
-                   '\scripts\tabela_plantas.txt'.
+           'C:\Users\tihso\OneDrive\Área de Trabalho\Floricultura' &
+               '\Floricultura-7\scripts\tabela_plantas.txt'.
                    
        01  STATUS-ARQUIVO.
            05 FS-ARQUIVO       PIC 9(02).
        
-       01  WS-ARQUIVO          PIC X(80).          
+       01  WS-ARQUIVO          PIC X(80).
+       
+       01  WS-REGISTRO.
+           05 WS-ID            PIC 9(01).
+           05 WS-NOME          PIC X(30).
+           05 WS-PRECO         PIC 9(8)V99.
+           05 WS-PREFERENCIA   PIC X(41).
+           
       *     
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
@@ -47,14 +58,30 @@
                
           LE-ARQUIVO.
                PERFORM UNTIL FS-ARQUIVO NOT = 00
-                   READ WS-ARQUIVO INTO WS-REGISTRO
+                   READ ARQUIVO INTO WS-ARQUIVO
                    AT END
-                       
-                   UNSTRING WS-ARQUIVO
-                       DELIMITED BY ';'
-                       INTO WS-REGISTRO
-                   END-UNSTRING    
+                       MOVE 99 TO FS-ARQUIVO
+                   NOT AT END
+                       PERFORM UNSTRING-ARQUIVO
+                       PERFORM MOSTRA-ARQUIVO
                END-PERFORM.    
+           
+          UNSTRING-ARQUIVO.
+               UNSTRING WS-ARQUIVO
+                   DELIMITED BY ';'
+                   INTO WS-ID
+                        WS-NOME
+                        WS-PRECO
+                        WS-PREFERENCIA
+              END-UNSTRING.
+           
+          MOSTRA-ARQUIVO.
+               DISPLAY '-------------------------------------------'
+               DISPLAY 'WS-ARQUIVO: 'WS-ARQUIVO.
+               DISPLAY 'ID: 'WS-ID.
+               DISPLAY 'NOME: 'WS-NOME.
+               DISPLAY 'PRECO: 'WS-PRECO.
+               DISPLAY 'PREFERENCIA: 'WS-PREFERENCIA.
                
           FECHA-ARQUIVO.
                CLOSE ARQUIVO.
@@ -65,4 +92,3 @@
                
                
        END PROGRAM FLORICULTURA_ARQUIVO.
-
