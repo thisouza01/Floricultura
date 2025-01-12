e562761c81043cfd547b0131423547d4ef37f6f0
Thiago Souza <124224124+thisouza01@users.noreply.github.com>
2025-01-12 13:06:38 -0300

    Update ler-arquivo.cob
    corrigindo output do preço

--- cobol/ler-arquivo.cob
+++ cobol/ler-arquivo.cob
@@ -6,48 +6,49 @@
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. FLORICULTURA_ARQUIVO.
-      * 
-       ENVIRONMENT DIVISION.   
-       CONFIGURATION SECTION.    
+      *
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
            SPECIAL-NAMES.
                DECIMAL-POINT IS COMMA.
-      * 
+      *
        INPUT-OUTPUT SECTION.
           FILE-CONTROL.
               SELECT ARQUIVO ASSIGN TO CAMINHO-ARQUIVO
               ORGANIZATION IS LINE SEQUENTIAL
               FILE STATUS IS FS-ARQUIVO.
-      * 
+      *
        DATA DIVISION.
        FILE SECTION.
          FD ARQUIVO.
          01 REGISTRO         PIC X(80).
-      * 
+      *
        WORKING-STORAGE SECTION.
        01  CAMINHO-ARQUIVO PIC X(100) VALUE
            'C:\Users\tihso\OneDrive\Área de Trabalho\Floricultura' &
                '\Floricultura-7\scripts\tabela_plantas.txt'.
-                   
+
        01  STATUS-ARQUIVO.
            05 FS-ARQUIVO       PIC 9(02).
-       
+
        01  WS-ARQUIVO          PIC X(80).
-       
+
        01  WS-REGISTRO.
            05 WS-ID            PIC 9(01).
            05 WS-NOME          PIC X(30).
            05 WS-PRECO         PIC 9(8)V99.
            05 WS-PREFERENCIA   PIC X(41).
-           
-      *     
-       PROCEDURE DIVISION.
+   
+       01 WS-PRECO-ED      PIC ZZZZZZZZ,ZZ.       
+      *
+         PROCEDURE DIVISION.
        MAIN-PROCEDURE.
            PERFORM ABRE-ARQUIVO.
            PERFORM LE-ARQUIVO.
            PERFORM FECHA-ARQUIVO.
            STOP RUN.
-           
-           
+
+
           ABRE-ARQUIVO.
                OPEN INPUT ARQUIVO.
                IF FS-ARQUIVO NOT = 00
@@ -55,7 +56,7 @@
                    STOP RUN
                ELSE
                    DISPLAY 'ARQUIVO ABERTO COM SUCESSO!'.
-               
+
           LE-ARQUIVO.
                PERFORM UNTIL FS-ARQUIVO NOT = 00
                    READ ARQUIVO INTO WS-ARQUIVO
@@ -63,9 +64,10 @@
                        MOVE 99 TO FS-ARQUIVO
                    NOT AT END
                        PERFORM UNSTRING-ARQUIVO
+                       PERFORM EDITA-PRECO
                        PERFORM MOSTRA-ARQUIVO
-               END-PERFORM.    
-           
+               END-PERFORM.
+
           UNSTRING-ARQUIVO.
                UNSTRING WS-ARQUIVO
                    DELIMITED BY ';'
@@ -74,21 +76,25 @@
                         WS-PRECO
                         WS-PREFERENCIA
               END-UNSTRING.
-           
+                  
+          EDITA-PRECO.
+              MOVE WS-PRECO TO WS-PRECO-ED.
+
           MOSTRA-ARQUIVO.
                DISPLAY '-------------------------------------------'
-               DISPLAY 'WS-ARQUIVO: 'WS-ARQUIVO.
+      * ARQUIVO COM TAMANHO FIXO DE 80 CARACTERES
+               DISPLAY  WS-REGISTRO.
                DISPLAY 'ID: 'WS-ID.
                DISPLAY 'NOME: 'WS-NOME.
-               DISPLAY 'PRECO: 'WS-PRECO.
+               DISPLAY 'PRECO: 'WS-PRECO-ED.
                DISPLAY 'PREFERENCIA: 'WS-PREFERENCIA.
-               
+
           FECHA-ARQUIVO.
                CLOSE ARQUIVO.
                IF FS-ARQUIVO NOT = 00
                  DISPLAY 'ERRO AO FECHAR O ARQUIVO. STATUS: ' FS-ARQUIVO
                ELSE
                  DISPLAY 'ARQUIVO FECHADO COM SUCESSO.'.
-               
-               
+
+
        END PROGRAM FLORICULTURA_ARQUIVO.
