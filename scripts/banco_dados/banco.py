17df993f291f915ada2f00f718aa0dbbee32989e
unknown <tihsouza01@gmail.com>
2025-01-15 10:28:06 -0300

    alterando output da listagem

--- scripts/banco_dados/banco.py
+++ scripts/banco_dados/banco.py
@@ -15,9 +15,9 @@ def receber_dados_bd():
 #                    INSERE PLANTAS NO BANCO DE DADOS
 
 def cadastrar_planta_db(conexao):
-
+    # dados ja vem validados
     resultado = recebe_dados()
-
+    
     nome_db = resultado[0]
     preco_db = resultado[1]
     preferencia_db = resultado[2]
@@ -47,13 +47,16 @@ def mostrar_plantas_db(conexao):
 
     with sqlite3.connect(conexao) as conecta:
 
+        conecta.row_factory = sqlite3.Row
+
         cursor = conecta.execute("SELECT ID, NOME, PRECO, PREFERENCIA FROM PLANTA")
 
         for linha in cursor:
-            print('----------------------')
-            print('ID = ', linha[0])
-            print('NOME = ', linha[1])
-            print('PRECO = ', linha[2])
+            print('-'*80)
+            print('ID = ', linha['ID'], end = ' | ')
+            print('NOME = ', linha['NOME'], end = ' | ')
+            print('PRECO = ', linha['PRECO'], end = ' | ')
+            print('PREFERENCIA = ', linha['PREFERENCIA'])
 
         print('Consulta realizado com sucesso!!')    
 
