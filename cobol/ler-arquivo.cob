9a82804ae97674b1e36a1627bf424aaf66562468
Thiago Souza <124224124+thisouza01@users.noreply.github.com>
2025-01-22 09:13:47 -0300

    Update ler-arquivo.cob
    atualizando registros

--- cobol/ler-arquivo.cob
+++ cobol/ler-arquivo.cob
@@ -24,33 +24,45 @@
          01 REGISTRO         PIC X(80).
       *
        WORKING-STORAGE SECTION.
-       01  CAMINHO-ARQUIVO PIC X(100) VALUE
-           'C:\Users\WIN 11\Floricultura\Floricultura-2' &
-               '\scripts\tabela_plantas.txt'.
+       01  CAMINHO-ARQUIVO PIC X(100) VALUE 'C:\Users\WIN 11\OneDrive' &
+               '\Desktop\Floricultura' &
+               '\Floricultura-4\scripts\tabela_plantas.txt'.
 
        01  STATUS-ARQUIVO.
            05 FS-ARQUIVO       PIC 9(02).
-
+       
        01  WS-ARQUIVO          PIC X(80).
 
+       77  QNT-PLANTAS         PIC 9(02) VALUE ZEROS.
+        
+       01  PLANTAS.
+           03 LIDOS            PIC 9(02)
+               OCCURS 1 TO 99 DEPENDING ON QNT-PLANTAS.
+               
+       77  I                   PIC 9(02) VALUE ZEROS.        
+           
+       01  TABELA-REGISTROS.
+           03 REGISTROS        PIC X(90)
+               OCCURS 1 TO 99 DEPENDING ON QNT-PLANTAS.
+           
        01  WS-REGISTRO.
-           05 WS-ID            PIC 9(01).
+           05 WS-ID            PIC 9(02).
            05 WS-NOME          PIC X(30).
-           05 WS-PRECO         PIC X(08).
+           05 WS-PRECO         PIC X(06).
            05 WS-PREFERENCIA   PIC X(41).
 
-       01 WS-PRECO-NUM         PIC 9(08)V99.    
-       01 WS-PRECO-ED          PIC ZZZZZZZ9,99.
-       
+       01 WS-PRECO-NUM         PIC 9(08)V99.
+       01 WS-PRECO-ED          PIC ZZZZZ9,99.
+
        01 REGISTRO-MOD.
-           05 ID-MOD           PIC 9(01).
+           05 ID-MOD           PIC 9(02).
            05 FILLER           PIC X(01) VALUE '|'.
            05 NOME-MOD         PIC X(29).
            05 FILLER           PIC X(01) VALUE '|'.
-           05 PRECO-MOD        PIC ZZZZZZZZ,ZZ.
+           05 PRECO-MOD        PIC ZZZZZZ,ZZ.
            05 FILLER           PIC X(01) VALUE '|'.
-           05 PREFERENCIA-MOD  PIC X(40).           
-           
+           05 PREFERENCIA-MOD  PIC X(40).
+
        01 EOF                  PIC X(01) VALUE 'N'.
        01 CABECALHO            PIC X(01) VALUE 'N'.
 
@@ -69,10 +81,13 @@
                            PERFORM CONVERTE-NUM
                            PERFORM EDITA-PRECO
                            PERFORM MOVE-REGISTRO
-                           PERFORM MOSTRA-ARQUIVO
+                           PERFORM CONTA-REGISTRO                           
+                           PERFORM MOVE-REGISTRO-PARA-TABELA
+                           PERFORM MOSTRA-TABELA
                        END-IF
                    END-IF
            END-PERFORM.
+           PERFORM QNT-REGISTRO-LIDO.    
            PERFORM FECHA-ARQUIVO.
            STOP RUN.
 
@@ -98,37 +113,53 @@
                         WS-PRECO
                         WS-PREFERENCIA
               END-UNSTRING.
-
+          
+          CONTA-REGISTRO.
+              IF QNT-PLANTAS < 99
+               COMPUTE QNT-PLANTAS = QNT-PLANTAS + 1
+              END-IF. 
+               
+          QNT-REGISTRO-LIDO.
+              DISPLAY 'FORAM LIDOS: ' QNT-PLANTAS ' REGISTROS'.
+           
           CONVERTE-NUM.
               INSPECT WS-PRECO CONVERTING '.' TO ','.
               MOVE FUNCTION NUMVAL-C(WS-PRECO) TO WS-PRECO-NUM.
-              
-              
+
+
           EDITA-PRECO.
               MOVE WS-PRECO-NUM TO WS-PRECO-ED.
-              
+
           MOVE-REGISTRO.
               MOVE WS-ID TO ID-MOD.
               MOVE WS-NOME TO NOME-MOD.
               MOVE WS-PRECO-NUM TO PRECO-MOD.
               MOVE WS-PREFERENCIA TO PREFERENCIA-MOD.
-
+           
+          MOVE-REGISTRO-PARA-TABELA.
+              MOVE REGISTRO-MOD TO TABELA-REGISTROS.
+              
+          MOSTRA-TABELA.
+              PERFORM VARYING I FROM 1 BY 1 UNTIL I > QNT-PLANTAS
+                   DISPLAY REGISTROS(I)
+              END-PERFORM.
+           
           MOSTRA-ARQUIVO.
-               DISPLAY '-------------------------------------------'
+               DISPLAY '-----------------------------------------------'
       * ARQUIVO COM TAMANHO FIXO DE 80 CARACTERES
                DISPLAY  REGISTRO-MOD.
-               DISPLAY 'ID: 'ID-MOD.
-               DISPLAY 'NOME: 'NOME-MOD.
-               DISPLAY 'PRECO: 'PRECO-MOD.
-               DISPLAY 'PREFERENCIA: 'PREFERENCIA-MOD.
+      *         DISPLAY 'ID: 'ID-MOD.
+      *         DISPLAY 'NOME: 'NOME-MOD.
+      *         DISPLAY 'PRECO: 'PRECO-MOD.
+      *         DISPLAY 'PREFERENCIA: 'PREFERENCIA-MOD.
 
           FECHA-ARQUIVO.
                CLOSE ARQUIVO.
                IF FS-ARQUIVO NOT = 00
                  DISPLAY 'ERRO AO FECHAR O ARQUIVO. STATUS: ' FS-ARQUIVO
                ELSE
-                 DISPLAY '-------------------------------------------'                   
-                 DISPLAY 'ARQUIVO FECHADO COM SUCESSO.'.
+                DISPLAY '----------------------------------------------'
+                DISPLAY 'ARQUIVO FECHADO COM SUCESSO.'.
 
 
        END PROGRAM FLORICULTURA_ARQUIVO.
