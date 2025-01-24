bfe25c60fdfcd3dfa65c78894c32d93df3efb19b
unknown <tihsouza01@gmail.com>
2025-01-24 16:57:08 -0300

    adicionando botoes pelo tkinter

--- scripts/layout.py
+++ scripts/layout.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-from banco_dados.banco import deleta_planta_db
+from banco_dados.banco import * 
 
 
 # cria uma nova instancia Tcl
@@ -12,13 +12,36 @@ frm = ttk.Frame(root, padding=10)
 # ajusta o Frame a janela root
 frm.grid()
 
-converter = deleta_planta_db(r'C:\Users\tihso\OneDrive\Área de Trabalho\Floricultura\Floricultura-18\scripts\teste.db')
+# BOTAO PARA ADICIONAR UMA PLANTA
+ttk.Button(
+    frm,
+    text='Adicionar',
+    command=lambda: cadastrar_planta_db(r'C:\Users\WIN 11\OneDrive\Desktop\Floricultura\Floricultura-7\scripts\teste.db')).grid(column=0, row=0)
 
-# cria uma Label que contem o texto Hello World!
-ttk.Button(frm, text='converter', command=converter).grid(column=0, row=0)
+# BOTAO PARA LISTAR TODAS AS PLANTA
+ttk.Button(
+    frm,
+    text='Listar todas',
+    command=lambda: mostrar_todas_plantas_db(r'C:\Users\WIN 11\OneDrive\Desktop\Floricultura\Floricultura-7\scripts\teste.db')).grid(column=0, row=1)
+
+# BOTAO PARA LISTAR UMA PLANTA
+ttk.Button(
+    frm,
+    text='Listar uma',
+    command=lambda: mostra_uma_planta_db(r'C:\Users\WIN 11\OneDrive\Desktop\Floricultura\Floricultura-7\scripts\teste.db')).grid(column=0, row=2)
+
+# BOTAO PARA ATUALIZAR UMA PLANTA
+ttk.Button(
+    frm,
+    text='Atualizar',
+    command=lambda: atualizar_planta_db(r'C:\Users\WIN 11\OneDrive\Desktop\Floricultura\Floricultura-7\scripts\teste.db')).grid(column=0, row=3)
+
 
 # cria um botal que sai da janela
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+ttk.Button(
+    frm,
+    text="Quit",
+    command=root.destroy).grid(column=3, row=0)
 
 # precisa de um loop para rodar a janela
 root.mainloop()
